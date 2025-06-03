8b544142841219850bde88bbadba7d7c3da02f10
M.M.Bhavesh.H.R.K <munaga.bhavesh@gmail.com>
2025-06-03 19:48:01 +0530

    Move index.html to root

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
@@ -10,3 +11,17 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+=======
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./feedback.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+>>>>>>> 44fc9a3 (Move index.html to root)
--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from sqlalchemy import Column, Integer, String, Text
 from database import Base
 
@@ -16,3 +17,23 @@ class Feedback(Base):
     liked = Column(Text, nullable=True)
     improve = Column(Text, nullable=True)
     comments = Column(Text, nullable=True)
+=======
+from sqlalchemy import Column, Integer, String, Text
+from database import Base
+
+class Feedback(Base):
+    __tablename__ = "feedback"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    CName = Column(String, nullable=True)
+    email = Column(String, nullable=False)
+    service = Column(String, nullable=False)
+    satisfaction = Column(Integer, nullable=False)
+    onTime = Column(String, nullable=False)
+    communication = Column(Integer, nullable=False)
+    recommend = Column(String, nullable=False)
+    liked = Column(Text, nullable=True)
+    improve = Column(Text, nullable=True)
+    comments = Column(Text, nullable=True)
+>>>>>>> 44fc9a3 (Move index.html to root)
